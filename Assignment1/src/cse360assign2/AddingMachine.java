86fceaeb5c60fbe48d6e7ef4f89bc51ab7542dbf
Garrett <Garrett@DESKTOP-A8D1QVQ>
2019-10-09 19:37:33 -0700

    Added methods to finish AddingMachine.

--- Assignment1/src/cse360assign2/AddingMachine.java
+++ Assignment1/src/cse360assign2/AddingMachine.java
@@ -2,31 +2,38 @@
  * Garrett Stanwood
  * CSE360 - 70641
  * Assignment 2
+ * 
+ * Adds or subtracts given values into a total value. 
+ * Keeps track of operations and can return the total and the string of operations.
  */
 package cse360assign2;
 
 public class AddingMachine {
 
 	private int total;
+	private String operations = "0";
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		operations = "0";
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total = total + value;
+		operations = operations + " + " + value;
 	}
 	
 	public void subtract (int value) {
-		
+		total = total - value;
+		operations = operations + " - " + value;
 	}
 		
 	public String toString () {
-		return "";
+		return operations;
 	}
 
 	public void clear() {
